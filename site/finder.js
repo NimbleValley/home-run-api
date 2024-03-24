e82b87b68a396274825cf7ff02de044c63543da3
Mason <mason.d.mcmanus@outlook.com>
2024-03-24 17:37:24 -0500

    Home run finder version 1 done

--- site/finder.js
+++ site/finder.js
@@ -1,6 +1,9 @@
 const startDateInput = document.getElementById("start-date-input");
 const endDateInput = document.getElementById("end-date-input");
 
+startDateInput.parentElement.style.display = "none";
+endDateInput.parentElement.style.display = "none";
+
 startDateInput.valueAsDate = new Date(2018, 0, 7);
 endDateInput.valueAsDate = new Date();
 
@@ -46,16 +49,42 @@ playerSearchTextInput.addEventListener("change", function () {
     }
 });
 
+const yearSelect = document.getElementById("year-select");
+yearSelect.addEventListener("click", function () {
+    let selectedYear = parseInt(yearSelect.value);
+    if (selectedYear == 1) {
+        startDateInput.parentElement.style.display = "flex";
+        endDateInput.parentElement.style.display = "flex";
+        return;
+    } else {
+        startDateInput.parentElement.style.display = "none";
+        endDateInput.parentElement.style.display = "none";
+    }
+
+    if (selectedYear == 0) {
+        startDateInput.valueAsDate = new Date(2018, 0, 7);
+        endDateInput.valueAsDate = new Date();
+        return;
+    }
+
+    startDateInput.valueAsDate = new Date(selectedYear, 0, 7);
+    endDateInput.valueAsDate = new Date(selectedYear, 11, 7);
+});
+
 const minDistanceInput = document.getElementById("min-distance-input");
 const maxDistanceInput = document.getElementById("max-distance-input");
 
 const awayTeamSelect = document.getElementById("away-team-select");
 const homeTeamSelect = document.getElementById("home-team-select");
 
+const outputContainer = document.getElementById("home-run-finder-output-container");
+
 const searchButton = document.getElementById("home-run-search-button");
 searchButton.addEventListener("click", search);
 
-function search() {
+async function search() {
+    outputContainer.replaceChildren();
+
     let startYear = parseInt(startDateInput.valueAsDate.getFullYear());
     let endYear = parseInt(endDateInput.valueAsDate.getFullYear());
 
@@ -73,40 +102,53 @@ function search() {
         endYear = 2024
     }
 
-    let minDistance = parseInt(minDistanceInput).value;
-    let maxDistance = parseInt(maxDistanceInput).value;
+    let minDistance = parseInt(minDistanceInput.value);
+    let maxDistance = parseInt(maxDistanceInput.value);
     if (minDistance > maxDistance) {
         let tempYear = minDistance;
         minDistance = maxDistance;
         maxDistance = tempYear;
     }
 
-    playerId = playerRows[parseInt(playerSelect.value)][10];
+    if (isNaN(parseInt(playerSelect.value))) {
+        playerId = -1;
+    } else {
+        playerId = playerRows[parseInt(playerSelect.value)][10];
+    }
 
     for (let year = startYear; year <= endYear; year++) {
         console.log(year);
-        Papa.parse(`./data/${year}.csv`, {
+        await Papa.parse(`./data/${year}.csv`, {
             download: true,
             complete: function (results) {
                 data = results.data;
 
-                for(let i = 1; i < data.length; i ++) {
-                    if(data[i][6] != playerId && playerId != -1) {
+                for (let i = 1; i < data.length; i++) {
+                    if (data[i][8] != "home_run") {
                         continue;
                     }
 
-                    if(parseInt(data[i][52]) < minDistance || parseInt(data[i][52]) > maxDistance) {
+                    if (data[i][6] != playerId && playerId != -1) {
                         continue;
                     }
 
-                    if(parseInt(awayTeamSelect.value) != 31 && awayTeamSelect.value != data[i][20]) {
-                        continue;
-                    }
-                    
-                    if(parseInt(homeTeamSelect.value) != 31 && homeTeamSelect.value != data[i][19]) {
+                    if (parseInt(data[i][52]) < minDistance || parseInt(data[i][52]) > maxDistance) {
                         continue;
                     }
 
+                    if (parseInt(awayTeamSelect.value) != 31 && awayTeamSelect.value != data[i][20]) {
+                        continue;
+                    }
+
+                    if (parseInt(homeTeamSelect.value) != 31 && homeTeamSelect.value != data[i][19]) {
+                        continue;
+                    }
+
+                    let tempContainer = document.createElement("div");
+                    tempContainer.className = "home-run-result-container";
+                    tempContainer.innerText = `${data[i][1]} - ${data[i][52]}' - ${data[i][20]} at ${data[i][19]}: ${data[i][15]}`;
+
+                    outputContainer.appendChild(tempContainer);
                 }
             }
         });
